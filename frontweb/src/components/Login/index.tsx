aced303e1c131ac89b93587749916145b6d34c3a
leonardo ferreira <leonardo.ferreira@objective.com.br>
2022-03-18 18:03:32 -0300

    Style Login card

--- frontweb/src/components/Login/index.tsx
+++ frontweb/src/components/Login/index.tsx
@@ -52,54 +52,60 @@ const Login = () => {
   };
 
   return (
-    <div className="base-card login-card">
-      <h1>LOGIN</h1>
+    <div className="base-card bg-secondary login-card">
+      <div className="title-container">
+        <h1>LOGIN</h1>
+      </div>
       {hasError && (
         <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
       )}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <input
-            {...register('username', {
-              required: 'Campo obrigatório',
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Email inválido',
-              },
-            })}
-            type="text"
-            className={`form-control base-input ${
-              errors.username ? 'is-invalid' : ''
-            }`}
-            placeholder="Email"
-            name="username"
-          />
-          <div className="invalid-feedback d-block">
-            {errors.username?.message}
+      <div className="form-container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputs-container">
+            <div className="login-input">
+              <input
+                {...register('username', {
+                  required: 'Campo obrigatório',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email inválido',
+                  },
+                })}
+                type="text"
+                className={`form-control base-input ${
+                  errors.username ? 'is-invalid' : ''
+                }`}
+                placeholder="Email"
+                name="username"
+              />
+              <div className="invalid-feedback d-block">
+                {errors.username?.message}
+              </div>
+            </div>
+            <div className="login-input">
+              <input
+                {...register('password', {
+                  required: 'Campo obrigatório',
+                })}
+                type="password"
+                className={`form-control base-input ${
+                  errors.password ? 'is-invalid' : ''
+                }`}
+                placeholder="Password"
+                name="password"
+              />
+              <div className="invalid-feedback d-block">
+                {errors.password?.message}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="mb-2">
-          <input
-            {...register('password', {
-              required: 'Campo obrigatório',
-            })}
-            type="password"
-            className={`form-control base-input ${
-              errors.password ? 'is-invalid' : ''
-            }`}
-            placeholder="Password"
-            name="password"
-          />
-          <div className="invalid-feedback d-block">
-            {errors.password?.message}
+          <div className="login-submit">
+            <button className="btn btn-tertiary">
+              <h6>FAZER LOGIN</h6>
+            </button>
           </div>
-        </div>
-        <div className="login-submit">
-          <button className="btn btn-primary">
-            <h6>FAZER LOGIN</h6>
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
