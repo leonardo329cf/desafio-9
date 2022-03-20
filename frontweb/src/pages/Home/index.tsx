3ea98e22b887cb432dc95b13de37eeff0f5243d0
leonardo329cf <leonardo329cf@gmail.com>
2022-03-20 19:23:34 -0300

    Add image card to home page

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,12 +1,19 @@
 import Login from '../../components/Login';
+import HomeImage from '../../assets/images/homeImage.svg';
 
 import './styles.css';
-
 
 const Home = () => {
   return (
     <>
       <div className="home-container">
+        <div className="home-image-card">
+          <div className="image-card-content">
+            <h1>Avalie Filmes</h1>
+            <h4>Diga o que você achou do seu filme favorito</h4>
+          </div>
+          <img src={HomeImage} alt="Home image" />
+        </div>
         <Login />
       </div>
     </>
